73988602b621ebc18fcbdc454510d6382f0c425c
Caleb Severn <calsev@gmail.com>
2016-12-01 09:07:32 -0900

    Added config header

---
+++ ork/detail/config.hpp
@@ -0,0 +1,16 @@
+/*
+This file is part of the ORK library.
+Full copyright and license terms can be found in the LICENSE.txt file.
+*/
+#pragma once
+
+#ifndef ORK_ORK_HPP
+#error This header can only be included from ork/ork.hpp!
+#endif
+
+
+namespace ork {
+
+
+
+}//namespace ork
