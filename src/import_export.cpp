cbf3c6764a7e5583b883394f8abc6678130344a0
Caleb Severn <calsev@gmail.com>
2017-05-01 19:01:14 -0800

    Added files for import/export

---
+++ src/import_export.cpp
@@ -0,0 +1,24 @@
+/*
+This file is part of the ORK library.
+Full copyright and license terms can be found in the LICENSE.txt file.
+*/
+#include"ork/import_export.hpp"
+
+#if ORK_USE_PUGI
+#include"pugixml.hpp"
+#endif
+
+#if ORK_USE_JSON
+#include"json/json.hpp"
+#endif
+
+
+namespace ork {
+namespace io {
+
+
+
+
+
+}//namespace io
+}//namespace ork
