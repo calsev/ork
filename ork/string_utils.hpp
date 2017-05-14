9ccb0173801240ca5c6d955af2e1dcb93e7604a4
Caleb Severn <calsev@gmail.com>
2017-05-13 17:29:41 -0800

    Added string equality comparisons

--- ork/string_utils.hpp
+++ ork/string_utils.hpp
@@ -9,8 +9,12 @@ Full copyright and license terms can be found in the LICENSE.txt file.
 namespace ork {
 
 
-ORK_ORK_EXT(size_t) str_length(const char*);
-ORK_ORK_EXT(size_t) str_length(const wchar_t*);
+ORK_ORK_EXT(size_t) str_length(const char*const);
+ORK_ORK_EXT(size_t) str_length(const wchar_t*const);
+
+
+ORK_ORK_EXT(bool) str_equal(const char*const, const char*const);
+ORK_ORK_EXT(bool) str_equal(const wchar_t*const, const wchar_t*const);
 
 
 }//namespace ork
--- src/string_utils.cpp
+++ src/string_utils.cpp
@@ -10,17 +10,31 @@ Full copyright and license terms can be found in the LICENSE.txt file.
 namespace ork {
 
 
-size_t str_length(const char*str) {
+size_t str_length(const char*const str) {
 	if(str == nullptr) {
 		ORK_THROW(ORK("String length called for null string"));
 	}
 	return std::strlen(str);
 }
-size_t str_length(const wchar_t*str) {
+size_t str_length(const wchar_t*const str) {
 	if(str == nullptr) {
 		ORK_THROW(ORK("String length called for null string"));
 	}
 	return std::wcslen(str);
+}
+
+
+bool str_equal(const char*const lhs, const char*const rhs) {
+	if(lhs == nullptr || rhs == nullptr) {
+		ORK_THROW(ORK("String length called for null string"));
+	}
+	return std::strcmp(lhs, rhs) == 0;
+}
+bool str_equal(const wchar_t*const lhs, const wchar_t*const rhs) {
+	if(lhs == nullptr || rhs == nullptr) {
+		ORK_THROW(ORK("String length called for null string"));
+	}
+	return std::wcscmp(lhs, rhs) == 0;
 }
 
 
