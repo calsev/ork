a8c40bc1cc314cc654a88d5ef96f61a2a9934ea8
Caleb Severn <calsev@gmail.com>
2017-05-13 16:47:38 -0800

    Added overloads for string length

--- ork/string_utils.hpp
+++ ork/string_utils.hpp
@@ -9,7 +9,8 @@ Full copyright and license terms can be found in the LICENSE.txt file.
 namespace ork {
 
 
-
+ORK_ORK_EXT(size_t) str_length(const char*);
+ORK_ORK_EXT(size_t) str_length(const wchar_t*);
 
 
 }//namespace ork
--- src/string_utils.cpp
+++ src/string_utils.cpp
@@ -2,13 +2,26 @@
 This file is part of the ORK library.
 Full copyright and license terms can be found in the LICENSE.txt file.
 */
+#include<cstring>
+#include<cwchar>
 #include"ork/string_utils.hpp"
 
 
 namespace ork {
 
 
-
+size_t str_length(const char*str) {
+	if(str == nullptr) {
+		ORK_THROW(ORK("String length called for null string"));
+	}
+	return std::strlen(str);
+}
+size_t str_length(const wchar_t*str) {
+	if(str == nullptr) {
+		ORK_THROW(ORK("String length called for null string"));
+	}
+	return std::wcslen(str);
+}
 
 
 }//namespace ork
