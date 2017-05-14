54684235948012f6d8034f8d35cea6a566304701
Caleb Severn <calsev@gmail.com>
2017-05-13 16:36:04 -0800

    Added files for string utils

---
+++ ork/string_utils.hpp
@@ -0,0 +1,15 @@
+/*
+This file is part of the ORK library.
+Full copyright and license terms can be found in the LICENSE.txt file.
+*/
+#pragma once
+#include"ork/ork.hpp"
+
+
+namespace ork {
+
+
+
+
+
+}//namespace ork
---
+++ src/string_utils.cpp
@@ -0,0 +1,14 @@
+/*
+This file is part of the ORK library.
+Full copyright and license terms can be found in the LICENSE.txt file.
+*/
+#include"ork/string_utils.hpp"
+
+
+namespace ork {
+
+
+
+
+
+}//namespace ork
