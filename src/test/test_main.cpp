667c4540235a8052c827a3d0c99b03d682ad7f45
Caleb Severn <calsev@gmail.com>
2017-12-30 14:16:23 -0900

    Added first test case

--- src/test/test_main.cpp
+++ src/test/test_main.cpp
@@ -3,9 +3,16 @@ This file is part of the ORK library.
 Full copyright and license terms can be found in the LICENSE.txt file.
 */
 #include"ork/ork.hpp"
+#include"ork/string_utils.hpp"
+
+#define CATCH_CONFIG_MAIN 1
+#include"catch.hpp"
 
 ORK_GLOBAL_LOG(ORK("../Logs"))
 
-int main(const int argc, const ork::char_t*const argv[]) {
-	return 0;
+using namespace ork;
+
+TEST_CASE("Strings are encoded", "[string]") {
+	const bstring test1{"Encode this 1!"};
+	REQUIRE(encode(reinterpret_cast<const unsigned char*>(test1.data()), test1.size(), encoding::base_64)=="RW5jb2RlIHRoaXMgMSE=");
 }
