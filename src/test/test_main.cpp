10e6d603be9bf2b2c929cca7d88e045d00e3b919
Caleb Severn <calsev@gmail.com>
2017-12-30 13:31:51 -0900

    Added placeholder for test_main

---
+++ src/test/test_main.cpp
@@ -0,0 +1,11 @@
+/*
+This file is part of the ORK library.
+Full copyright and license terms can be found in the LICENSE.txt file.
+*/
+#include"ork/ork.hpp"
+
+ORK_GLOBAL_LOG(ORK("../Logs"))
+
+int main(const int argc, const ork::char_t*const argv[]) {
+	return 0;
+}
