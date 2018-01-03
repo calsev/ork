31bff38ad3479431ef033e5a6ad2d4a36c3c0e3d
Caleb Severn <calsev@gmail.com>
2018-01-02 19:40:37 -0900

    Added missing ork scopes to logging macros

--- ork/core/log.hpp
+++ ork/core/log.hpp
@@ -118,11 +118,12 @@ ORK_ORK_EXT(logger&) get_global_log();
 #define ORK_GLOBAL_LOG(LOG_DIRECTORY) const int g_ork_log_val = ork::make_global_log(LOG_DIRECTORY);
 
 
-#define _ORK_LOG(CH, SV, UID) auto UID{get_global_log().get_log_scope(ORK_FILEN, ORK_LINE, ORK_FUNC, CH, SV)}; UID
-#define ORK_LOG(SV) _ORK_LOG(log_channel::debug_trace, SV, ORK_UID(_ork_log_log_))
-#define ORK_LOUT _ORK_LOG(log_channel::output_data, severity_level::info, ORK_UID(_ork_log_out_)
+#define _ORK_LOG(CH, SV, UID) auto UID{ork::get_global_log().get_log_scope(ORK_FILEN, ORK_LINE, ORK_FUNC, CH, SV)}; UID
+#define ORK_LOG(SV) _ORK_LOG(ork::log_channel::debug_trace, SV, ORK_UID(_ork_log_log_))
+#define ORK_LOUT _ORK_LOG(ork::log_channel::output_data, ork::severity_level::info, ORK_UID(_ork_log_out_)
 
 #define ORK_LOC_BLOCK ORK("\n -- ") << ORK_FLOC_STREAM << ORK(":\n -- ") << ORK_FUNC  << ORK("\n")
+
 
 }//namespace ork
 
