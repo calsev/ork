75622ce005c05526d10f8d44b439f0e6fc042a28
Caleb Severn <calsev@gmail.com>
2018-01-11 19:47:10 -0900

    Added a test file for color

---
+++ src/test/color.cpp
@@ -0,0 +1,14 @@
+/*
+This file is part of the ORK library.
+Full copyright and license terms can be found in the LICENSE.txt file.
+*/
+#include"ork/color.hpp"
+
+#include"ork/test/catch_include.hpp"
+
+#include"glm/vec4.hpp"
+
+using namespace ork;
+
+TEST_CASE("Grey round trip to hex", "[color]") {
+}
