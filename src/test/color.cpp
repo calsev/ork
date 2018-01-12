68fb2e29a4d339a33b87ba8b9d0e7b1c2953f346
Caleb Severn <calsev@gmail.com>
2018-01-11 20:15:55 -0900

    Added a test case for rgb to hex

--- src/test/color.cpp
+++ src/test/color.cpp
@@ -16,4 +16,11 @@ TEST_CASE("Convention for hue", "[color]") {
 	const color4 r1{0.f, 0.f, 0.f, 1.f};
 	REQUIRE(truncate_hue(d1, color_space::rgb) == r1);
 }
+
+TEST_CASE("RGB round trip to HEX", "[color]") {
+	const color4 d1{0.1, 0.2, 0.3, 0.4};
+	const string r1{ORK("1A334D66")};
+	const string to{to_hex(d1, color_space::rgb)};
+	REQUIRE(to == r1);
+}
 }
