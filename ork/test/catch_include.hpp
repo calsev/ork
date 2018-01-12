d5ad3bb2f6d37d7e8b1ec91cd96ebb73baff1312
Caleb Severn <calsev@gmail.com>
2018-01-11 19:38:53 -0900

    Disabled warning for catch

--- ork/test/catch_include.hpp
+++ ork/test/catch_include.hpp
@@ -10,6 +10,7 @@ Full copyright and license terms can be found in the LICENSE.txt file.
 #pragma warning(disable:4061) //enum val not explicitly handled by a case label
 #pragma warning(disable:4365) //return' signed/unsigned mismatch
 #pragma warning(disable:4371) //layout changed
+#pragma warning(disable:4583) //destructor is not implicitly called
 #pragma warning(disable:4623) //default constructor implicitly deleted
 #pragma warning(disable:4625) //implicit copy constructor
 #pragma warning(disable:4626) //implicit copy assignment
