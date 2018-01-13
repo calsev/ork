72ce251236f75aa63025c42f45ae91e866785115
Caleb Severn <calsev@gmail.com>
2018-01-12 19:07:19 -0900

    Added assertions for rgb to hsl

--- src/test/color.cpp
+++ src/test/color.cpp
@@ -34,18 +34,44 @@ bool within_3(const color4& v1, const color4& v2) {
 	}
 	return true;
 }
+bool within_5(const float v1, const float v2) {
+	return std::abs(v1 - v2) < 0.00001;
+}
+bool within_5(const color4& v1, const color4& v2) {
+	LOOPVIG(v1) {
+		if(!within_3(v1[i], v2[i])) {
+			return false;
+		}
+	}
+	return true;
+}
 
 
 TEST_CASE("Convention for hue", "[color]") {
-	const color4 d1{0.f, 0.f, 0.f, 1.f};
-	const color4 r1{0.f, 0.f, 0.f, 1.f};
+	const color4 d1{0.0, 0.0, 0.0, 1.0};
+	const color4 r1{0.0, 0.0, 0.0, 1.0};
 	REQUIRE(truncate_hue(d1, color_space::rgb) == r1);
 }
 
-TEST_CASE("RGB Conversion", "[color]") {
-	const color4 rgb{0.2f, 0.3f, 0.4f, 0.5f};
-	const color4 r1{0.5850f, 0.3333f, 0.3000f, 0.5f};//HSL
-	const color4 r2{0.5850f, 0.5000f, 0.4000f, 0.5f};//HSV
+
+TEST_CASE("RGB To HSL", "[color]") {
+	const color4 red_hsl_e{0.0, 1.0, 0.5, 1.0};
+	const color4 green_hsl_e{1.0 / 3.0, 1.0, 0.5, 1.0};
+	const color4 blue_hsl_e{2.0 / 3.0, 1.0, 0.5, 1.0};
+	const color4 white_hsl_e{0.0, 0.0, 1.0, 1.0};
+	const color4 black_hsl_e{0.0, 0.0, 0.0, 1.0};
+
+	const color4 red_hsl_r{convert(red, color_space::rgb, color_space::hsl)};
+	const color4 green_hsl_r{convert(green, color_space::rgb, color_space::hsl)};
+	const color4 blue_hsl_r{convert(blue, color_space::rgb, color_space::hsl)};
+	const color4 white_hsl_r{convert(white, color_space::rgb, color_space::hsl)};
+	const color4 black_hsl_r{convert(black, color_space::rgb, color_space::hsl)};
+
+	REQUIRE(within_5(red_hsl_e, red_hsl_r));
+	REQUIRE(within_5(green_hsl_e, green_hsl_r));
+	REQUIRE(within_5(blue_hsl_e, blue_hsl_r));
+	REQUIRE(within_5(white_hsl_e, white_hsl_r));
+	REQUIRE(within_5(black_hsl_e, black_hsl_r));
 	const color4 c1{convert(rgb, color_space::rgb, color_space::hsl)};
 	const color4 c2{convert(rgb, color_space::rgb, color_space::hsv)};
 }
