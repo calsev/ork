f961e702af3d86eea9dd387574dd12bb0fabaf2d
calsev <calsev@gmail.com>
2018-01-19 12:02:26 -0900

    Moved comments to ReadMe file

--- test/ork_test_local.bat
+++ test/ork_test_local.bat
@@ -1,19 +1,3 @@
-:: This script runs the ork test suite locally.
-:: Because ork builds and uses sibling dependencies, this requires some environment setup.
-:: The environment created here must be reproduced in appveyor for automated tests to work.
-:: This script assumes a local repo setup exists that is functionally equivalent to that below.
-::
-:: repo_root
-:: |-boost_141 (git repo)
-:: |-catch (git repo)
-:: |-glm (git repo)
-:: |-json (git repo)
-:: |-ork (git repo)
-:: |-ork_test_141 (created here)
-::   |-bin (created here)
-::   |-build (created here)
-:: |-pugi (git repo)
-:: |-yaml (git repo)
 
 :: These variables must match the appveyor environment
 :: We are only running one configuration for now
