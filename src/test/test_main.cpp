87d2433449c2f56ae3b26d7302a101c71057fca8
Caleb Severn <calsev@gmail.com>
2018-01-25 13:12:59 -0900

    Added generation of get args macro

--- src/test/test_main.cpp
+++ src/test/test_main.cpp
@@ -13,8 +13,21 @@ ORK_GLOBAL_LOG(ORK("../Logs"))
 using namespace ork;
 
 TEST_CASE("Strings are encoded", "[string]") {
-	const bstring test_1{"Encode this 1!"};
-	const bstring result_1{"RW5jb2RlIHRoaXMgMSE="};
+	const bstring test_1{BORK("Encode this 1!")};
+	const bstring result_1{BORK("RW5jb2RlIHRoaXMgMSE=")};
 	REQUIRE(encode(reinterpret_cast<const unsigned char*>(test_1.data()), test_1.size(), encoding::base_64)== result_1);
 	REQUIRE(decode(result_1, encoding::base_64) == test_1);
+}
+
+//We to avoid a second project to generate code
+TEST_CASE("Macro Generation", "[non_test]") {
+	static const size_t macro_max = 40;
+	ORK_FILE_WRITE_B(ORK("generated.cpp"));
+	LOOPI(macro_max) {
+		fout << BORK("#define ORK_GET_ARG_") << (i / 10 > 0 ? BORK("") : BORK("0")) << i << BORK("_(");
+		LOOPJ(i + 1) {
+			fout << BORK('A') << j << BORK(", ");
+		}
+		fout << BORK("...) A") << i << BORK('\n');
+	}
 }
