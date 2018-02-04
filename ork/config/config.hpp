ed130ead4b76b8d51b3d8393b96a18fe2f11488c
Caleb Severn <calsev@gmail.com>
2018-02-04 11:12:48 -0900

    Clang formatted config header

--- ork/config/config.hpp
+++ ork/config/config.hpp
@@ -3,55 +3,55 @@ This file is part of the ORK library.
 Full copyright and license terms can be found in the LICENSE.txt file.
 */
 #ifdef ORK_CONFIG_HPP
-#	error This header can only be included from ork/ork.hpp!
+#    error This header can only be included from ork/ork.hpp!
 #endif
 
 #ifndef ORK_CONFIG_HPP
-#define ORK_CONFIG_HPP
+#    define ORK_CONFIG_HPP
 
-#ifndef ORK_ORK_HPP
-#	error This header can only be included from ork/ork.hpp!
-#endif
+#    ifndef ORK_ORK_HPP
+#        error This header can only be included from ork/ork.hpp!
+#    endif
 
 
-#ifndef ORK_ORK_DLL//Shared library configuration
-#	define ORK_ORK_DLL 1
-#endif
+#    ifndef ORK_ORK_DLL // Shared library configuration
+#        define ORK_ORK_DLL 1
+#    endif
 
-#ifndef ORK_AUTO_LINK//Auto link ork library
-#	define ORK_AUTO_LINK 1
-#endif
+#    ifndef ORK_AUTO_LINK // Auto link ork library
+#        define ORK_AUTO_LINK 1
+#    endif
 
-#ifndef ORK_AUTO_LINK_3P//Auto link third-party dependencies
-#	define ORK_AUTO_LINK_3P ORK_AUTO_LINK
-#endif
+#    ifndef ORK_AUTO_LINK_3P // Auto link third-party dependencies
+#        define ORK_AUTO_LINK_3P ORK_AUTO_LINK
+#    endif
 
-#ifndef ORK_USE_BOOST//Optional dependency
-#	define ORK_USE_BOOST 1
-#endif
+#    ifndef ORK_USE_BOOST // Optional dependency
+#        define ORK_USE_BOOST 1
+#    endif
 
-#ifndef ORK_USE_GLM//Optional dependency
-#	define ORK_USE_GLM 1
-#endif
+#    ifndef ORK_USE_GLM // Optional dependency
+#        define ORK_USE_GLM 1
+#    endif
 
-#ifndef ORK_USE_JSON//Optional dependency
-#	define ORK_USE_JSON 1
-#endif
+#    ifndef ORK_USE_JSON // Optional dependency
+#        define ORK_USE_JSON 1
+#    endif
 
-#ifndef ORK_USE_PUGI//Optional dependency
-#	define ORK_USE_PUGI 1
-#endif
+#    ifndef ORK_USE_PUGI // Optional dependency
+#        define ORK_USE_PUGI 1
+#    endif
 
-#ifndef ORK_USE_YAML//Optional dependency
-#	define ORK_USE_YAML 1
-#endif
+#    ifndef ORK_USE_YAML // Optional dependency
+#        define ORK_USE_YAML 1
+#    endif
 
-#ifndef ORK_USE_ACIS//Configures numerical stuff
-#   define ORK_USE_ACIS 0
-#endif
+#    ifndef ORK_USE_ACIS // Configures numerical stuff
+#        define ORK_USE_ACIS 0
+#    endif
 
-#ifndef ORK_USE_OCC//Configures numerical stuff
-#   define ORK_USE_OCC 0
-#endif
+#    ifndef ORK_USE_OCC // Configures numerical stuff
+#        define ORK_USE_OCC 0
+#    endif
 
 #endif ORK_CONFIG_HPP
--- ork/core/boost_include.inl
+++ ork/core/boost_include.inl
@@ -1,23 +1,23 @@
-//No include guard!
+// No include guard!
 
 #if ORK_MSC
-#pragma warning(push)
-#pragma warning(disable:4365) //return' signed/unsigned mismatch
-#pragma warning(disable:4371) //layout changed
-#pragma warning(disable:4571) //catch(...) semantics changed for structured exceptions
-#pragma warning(disable:4619) //disable non-existant warning
-#pragma warning(disable:4625) //implicit copy constructor
-#pragma warning(disable:4626) //implicit copy assignment
-#pragma warning(disable:4668) //undefined macro converted to 0
-#pragma warning(disable:4774) //format string argument is not a string literal
-#pragma warning(disable:5026) //implicit move constructor
-#pragma warning(disable:5027) //implicit move assignment
-#pragma warning(disable:5031) //unmatched pragma push in header
+#    pragma warning(push)
+#    pragma warning(disable : 4365) // return' signed/unsigned mismatch
+#    pragma warning(disable : 4371) // layout changed
+#    pragma warning(disable : 4571) // catch(...) semantics changed for structured exceptions
+#    pragma warning(disable : 4619) // disable non-existant warning
+#    pragma warning(disable : 4625) // implicit copy constructor
+#    pragma warning(disable : 4626) // implicit copy assignment
+#    pragma warning(disable : 4668) // undefined macro converted to 0
+#    pragma warning(disable : 4774) // format string argument is not a string literal
+#    pragma warning(disable : 5026) // implicit move constructor
+#    pragma warning(disable : 5027) // implicit move assignment
+#    pragma warning(disable : 5031) // unmatched pragma push in header
 #endif
 
 #include ORK_BOOST_INC_FILE
 #undef ORK_BOOST_INC_FILE
 
 #if ORK_MSC
-#pragma warning(pop)
+#    pragma warning(pop)
 #endif
