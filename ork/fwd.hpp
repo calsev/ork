d47dc1c3016dbdb5f8aa29aacbf5ebde299006fc
Caleb Severn <calsev@gmail.com>
2018-02-12 13:09:00 -0900

    Added fwd file

---
+++ ork/fwd.hpp
@@ -0,0 +1,17 @@
+/*
+This file is part of the ORK library.
+Full copyright and license terms can be found in the LICENSE.txt file.
+*/
+#pragma once
+#include "ork/ork.hpp"
+
+#if ORK_USE_GLM
+#    include "glm/fwd.hpp"
+#endif // ORK_USE_GLM
+
+namespace ork {
+
+
+
+
+} // namespace ork
