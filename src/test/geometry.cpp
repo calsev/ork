6af5b86af3eee7655f2873cab22c033e6376068a
Caleb Severn <calsev@gmail.com>
2018-02-13 09:50:17 -0900

    Added a file for geometry test

---
+++ src/test/geometry.cpp
@@ -0,0 +1,13 @@
+/*
+This file is part of the ORK library.
+Full copyright and license terms can be found in the LICENSE.txt file.
+*/
+#include"ork/color.hpp"
+#include"ork/glm.hpp"
+
+#include"ork/test/catch_include.hpp"
+
+#include"glm/vec3.hpp"
+
+using namespace ork;
+
